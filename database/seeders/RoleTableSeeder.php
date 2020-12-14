8a5bbd08573399822e73432f7b8e234788e7241b
NaimBiswas <nayeembiswas2@gmail.com>
2020-12-14 19:03:15 +0600

    Create Reltion Between User.php and Role.php

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleTableSeeder extends Seeder
 {
@@ -13,6 +14,17 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('roles')->insert([
+            'name' => 'Admin',
+            'slug' => 'admin'
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'Author',
+            'slug' => 'author'
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'User',
+            'slug' => 'user'
+        ]);
     }
 }
--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserTableSeeder extends Seeder
 {
@@ -13,6 +14,29 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Md. Admin',
+            'role_id' => 1,
+            'username' => 'admin',
+            'email' => 'admin@blog.com',
+            'password' => bcrypt('admin@blog.com'),
+            'image' => 'defult.png',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Md. Author',
+            'role_id' => 2,
+            'username' => 'author',
+            'email' => 'author@blog.com',
+            'password' => bcrypt('author@blog.com'),
+            'image' => 'defult.png',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Md. User',
+
+            'username' => 'user',
+            'email' => 'user@blog.com',
+            'password' => bcrypt('user@blog.com'),
+            'image' => 'defult.png',
+        ]);
     }
 }
