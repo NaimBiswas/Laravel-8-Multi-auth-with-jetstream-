82da8966d519bc25e582d11083a4c80989da407e
NaimBiswas <nayeembiswas2@gmail.com>
2020-12-14 19:13:59 +0600

    User Tablse Error Fix and database migrate seeded

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->strin('role_id')->default('3');
+            $table->integer('role_id')->default('3');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('username')->unique()->nullable();
             $table->string('image')->nullable();
-            $table->strign('about')->nullable();
+            $table->string('about')->nullable();
             $table->string('password');
 
             $table->timestamp('email_verified_at')->nullable();
